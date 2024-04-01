7917546dfa7bc9f0268d72424239e4a48b8a66bb
Sachal Khalid <91531578+SachalKhalid@users.noreply.github.com>
2024-04-01 22:08:44 +0500

    Time: 34 ms (61.79%) | Memory: 16.6 MB (82.28%) - LeetSync

---
+++ Easy/58-length-of-last-word/length-of-last-word.py
@@ -0,0 +1,11 @@
+class Solution:
+    def lengthOfLastWord(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        wordlist = s.split()
+        if wordlist:
+            return len(wordlist[-1])
+        return 0
+        
